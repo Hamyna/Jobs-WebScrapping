0612b9ce3a64f5b4dad91a52345a4373eff49a40
Aminah <hamyna@hamyna-Inspiron-5720.(none)>
2014-08-07 16:44:51 -0700

    first phase of job webscrapping complete

--- Jobs-API/careerBuilder.py
+++ Jobs-API/careerBuilder.py
@@ -1,30 +1,65 @@
-#This script gets job posting from github
+# import library
 import urllib2
 import json
 import csv
 from time import sleep
 from lxml import etree
 from xml.etree import ElementTree as ET
+from optparse import OptionParser
+from xml.dom.minidom import parseString
 
 
-def get_jobposting():
-    # tell computer where to put CSV
-    outfile_path='/home/hamyna/Documents/Git/BullFrogIT/Jobs-WebsScrapping/Results/CareerBuilder/jobPosting_careerBuilder-IT.csv'
+def get_jobPosting():
+    headers = ['Company', 'CompanyDID', 'CompanyDetailsURL', 'DID', 'OnetCode', 'ONetFriendlyTitle',
+               'DescriptionTeaser', 'Distance', 'EmploymentType', 'EducationRequired',
+               'ExperienceRequired', 'JobDetailsURL', 'JobServiceURL', 'Location', 'LocationLatitude',
+               'LocationLongitude', 'PostedTime', 'Pay', 'SimilarJobsURL', 'JobTitle', 'ApplyRequirement',
+               'Skills']
+    # create a list with headings for our columns
+    #
+    outfile_path = '/home/hamyna/Documents/Git/BullFrogIT/Jobs-WebScrapping/Results/CareerBuilder/jobpostings-careerBuild.csv'
     # open it up, the w means we will write to it
-    writer = csv.writer(open(outfile_path, 'w'))
-    #create a list with headings for our columns
-    headers = ['Company', 'CompanyDetailsURL', 'CompanyDID', 'DescriptionTeaser', 'EmploymentType', 'EducationRequired', 'ExperienceRequired', 'Location', 'JobTitle', 'CanBulkApply', 'Skills']
-    url = urllib2.Request('http://api.careerbuilder.com/v1/jobsearch?DeveloperKey=WDHL7MB628RRNH2V6WJQ&Keywords=IT')
-    #it,technology,developer,software, programmer.
-    root = ET.parse(urllib2.urlopen(url)).getroot()
-    print root
-    #run through each item in results, and jump to an item in that dictionary, ex: the text of the tweet
-    items = root.findall('Results/JobSearchResult')
-    for item in items:
-         row = []
-         row.append(str(item['Company'].encode('utf-8')))
-         row.append(str(item['CompanyDetailsURL'].encode('utf-8')))
-         row.append(str(item['CompanyDID'].encode('utf-8')))
+    writer = csv.writer(open(outfile_path, 'wb'))
+    writer.writerow(headers)
+    #'developer', 'Programmer', 'IT', 'Technology', 'Application', 'Computer', 'Systems', 'tester', 'front-end', 'programming', 'software-test', 'Software', 'Data', 'hardware', 'mysql', 'engineer'
+    #'object-oriented-design', 'architect', 'object-oriented-design', 'architect', 'database', 'web', 'technician', 'analyst', 'mobile'
+    keywords = ['Java', '.NET', 'Python', 'CSS', 'mysql', 'network', 'html', 'security', 'iOS', 'developer', 'Programmer', 'IT', 'Technology',
+                'Application', 'Computer', 'Systems', 'tester', 'front-end', 'programming', 'software-test', 'Software', 'Data', 'hardware', 'mysql',
+                'engineer', 'object-oriented-design', 'architect', 'object-oriented-design', 'architect', 'database', 'web', 'technician', 'analyst', 'mobile']
+    #loop through the keyword and call the API then download the file:
+    for keyword in keywords:
+        print 'Downloading job post with keyword ' + keyword
+        pg = 1
+        while pg < 3:
+            print "Downloading page number" + str(pg)
+            #download data from API call to file.
+            file = urllib2.urlopen(
+                'http://api.careerbuilder.com/v1/jobsearch?DeveloperKey=WDHL7MB628RRNH2V6WJQ&Keywords=' + keyword + '&pg=' + str(
+                    pg))
+            #convert data in file to string:
+            data = file.read()
+            #close file because we don't need it anymore:
+            file.close()
+            i = 1
+            while i <= 24:
+                row = []
+                print 'downloading jop post number ' + str(i)
+                for column in headers:
+                    try:
+                        print 'downloading row ' + column
+                        dom = parseString(data)
+                        row.append(str(
+                            dom.getElementsByTagName(column)[i].toxml().replace(column, '').replace(
+                                column, '')))
+                    except UnicodeEncodeError as detail:
+                        print str(
+                            Exception) + "occured at row " + column + "with the keyword " + keyword + "page number " + str(pg)
+                        print detail
+                writer.writerow(row)
+                i = i + 1
+            pg = pg + 1
+            sleep(5)
+
 
 if __name__ == "__main__":
-    get_jobposting()
+    get_jobPosting()
--- Jobs-API/gitAPI.py
+++ Jobs-API/gitAPI.py
@@ -11,12 +11,12 @@ from time import sleep
 
 def get_jobposting():
     # tell computer where to put CSV
-    outfile_path='/home/hamyna/Documents/Git/BullFrogIT/Jobs-WebsScrapping/Results/Github/jobPosting_gitHub.csv'
+    outfile_path='/home/hamyna/Documents/Git/BullFrogIT/Jobs-WebScrapping/Results/Github/jobPosting_gitHub-.csv'
     # open it up, the w means we will write to it
     writer = csv.writer(open(outfile_path, 'w'))
     #create a list with headings for our columns
-    headers = ['title', 'location', 'type', 'description', 'company', 'how_to_apply']
-
+    headers = ['id', 'title', 'created_at', 'location', 'type', 'description', 'company', 'how_to_apply', 'url']
+    location = ['Atlanta', 'boston', 'bristol', 'brooklyn', 'CA', 'chicago', 'DC', 'LA', 'london', 'MA', 'Maryland', 'NC', 'NY', 'TORONTO', 'WATERLOO', 'Washington']
     #write the row of headings to our CSV file
     writer.writerow(headers)
     i=1 #iteration count
@@ -27,7 +27,7 @@ def get_jobposting():
         print i
         #create the URL for the json data, location is north america and page number is iteration i
         #and set the number of tweets per JSON file to the max of 5, so we have to do as little looping as possible
-        url = urllib2.Request('https://jobs.github.com/positions.json?description&location=north+america&page=' + str(i))
+        url = urllib2.Request('https://jobs.github.com/positions.json?description&location=TORONTO&page=' + str(i))
         #use the JSON library to turn this file into a Pythonic data structure
         parsed_json = json.load(urllib2.urlopen(url))
 
@@ -38,12 +38,15 @@ def get_jobposting():
          #initialize the row
          row = []
          #add every 'cell' to the row list, identifying the item just like an index in a list
+         row.append(str(posting['id'].encode('utf-8')))
          row.append(str(posting['title'].encode('utf-8')))
+         row.append(str(posting['created_at'].encode('utf-8')))
          row.append(str(posting['location'].encode('utf-8')))
          row.append(str(posting['type'].encode('utf-8')))
          row.append(str(posting['description'].encode('utf-8')))
          row.append(str(posting['company'].encode('utf-8')))
          row.append(str(posting['how_to_apply'].encode('utf-8')))
+         row.append(str(posting['url'].encode('utf-8')))
          #once you have all the cells in there, write the row to your csv
          writer.writerow(row)
         #increment our loop counter, now we're on the next time through the loop
